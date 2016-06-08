efdac7ce66907541f3e25247225cae9608c6cf1b
Benoit Orihuela <bobeal@gmail.com>
2016-06-08 11:36:53 +0200

    [Temp] Externalize CKAN's resources ids

--- src/main/java/org/ozwillo/dcexporter/service/DCModelMappingInitializer.java
+++ src/main/java/org/ozwillo/dcexporter/service/DCModelMappingInitializer.java
@@ -5,6 +5,7 @@ import org.ozwillo.dcexporter.model.DcModelMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -23,6 +24,12 @@ public class DCModelMappingInitializer implements ApplicationListener<Applicatio
     @Autowired
     private DcModelMappingRepository dcModelMappingRepository;
 
+    @Value("${ckan.organizationsResourceId}")
+    private String organizationsResourceId;
+
+    @Value("${ckan.poisResourceId}")
+    private String poisResourceId;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         if (dcModelMappingRepository.count() > 0) {
@@ -35,13 +42,13 @@ public class DCModelMappingInitializer implements ApplicationListener<Applicatio
         DcModelMapping orgMapping = new DcModelMapping("http://data.ozwillo.com/dc/type/dcmo:model_0/org:Organization_0",
             "org_1", "org:Organization_0", "Organisations");
         orgMapping.setCkanPackageId("organisations");
-        orgMapping.setCkanResourceId("f7d1d5dc-45c3-48fc-bca4-d9d98ba50d3a");
+        orgMapping.setCkanResourceId(organizationsResourceId);
         dcModelMappingRepository.save(orgMapping);
 
         DcModelMapping poiMapping = new DcModelMapping("http://data.ozwillo.com/dc/type/dcmo:model_0/poi:Geoloc_0",
             "poi_0", "poi:Geoloc_0", "Points d'intérêt");
         poiMapping.setCkanPackageId("points-interet-poi");
-        poiMapping.setCkanResourceId("b4fca7f7-773a-4bca-87f0-f54437082817");
+        poiMapping.setCkanResourceId(poisResourceId);
         dcModelMappingRepository.save(poiMapping);
     }
 }
