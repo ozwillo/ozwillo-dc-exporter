f42e37f9a54da3dcffd409e3ede8237d3b94e326
Benoit Orihuela <bobeal@gmail.com>
2016-08-03 08:15:39 +0200

    Display models sorted

--- src/main/java/org/ozwillo/dcexporter/controller/DatacoreController.java
+++ src/main/java/org/ozwillo/dcexporter/controller/DatacoreController.java
@@ -2,7 +2,6 @@ package org.ozwillo.dcexporter.controller;
 
 import org.oasis_eu.spring.datacore.model.DCModel;
 import org.ozwillo.dcexporter.service.DatacoreService;
-import org.ozwillo.dcexporter.service.SynchronizerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/dc")
@@ -18,11 +18,8 @@ public class DatacoreController {
     @Autowired
     private DatacoreService datacoreService;
 
-    @Autowired
-    private SynchronizerService synchronizerService;
-
     @RequestMapping(value = "/models", method = RequestMethod.GET)
     public List<DCModel> getModels() {
-        return datacoreService.getModels();
+        return datacoreService.getModels().stream().sorted().collect(Collectors.toList());
     }
 }
