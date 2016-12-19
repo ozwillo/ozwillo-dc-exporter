22797d64ebf27f96a35a3add384a1a0ce1a5046c
OtakuDigitChoc <abdelaliboulajine@gmail.com>
2016-12-19 17:21:15 +0100

    enhancement/#3-configuration screen of mapping from DC to CKAN
    add project chooser in form datasetsAdder
    
    get ckanDataset id and add all data in dcModelMapping
    
    CKAN tags list
    
    statement tag list on dc-exporter web service
    
    get a tag list in interface datasetAdder
    
    add package react-tag-input in dependence
    
    add tag field with suggestion from CKAN
    
    stylesheet for tags field
    
    structuring code ProjectChooser React component
    
    structuring code React Component TagAutocomplet
    
    add new fields in DcModelMapping
    
    add new fields in datasetAdder interface
    
    add source field and fix projectChooser
    
    fix problem of adding value in tags state
    
    add new field in datasetAdder interface for ckan
    
    add propertie in dcModelMapping
    
    remove propertie tags for test
    
    add of property tags in DCModel
    
    autocomplete tags
    
     Adding infromation to send to ckan
    
    fix sending problem of tags
    
    data successfully transferred in CKAN
    
    patches of pull requests [#13]

--- src/main/java/org/ozwillo/dcexporter/service/CkanService.java
+++ src/main/java/org/ozwillo/dcexporter/service/CkanService.java
@@ -7,6 +7,7 @@ import eu.trentorise.opendata.jackan.model.*;
 import org.joda.time.LocalDateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
+import org.ozwillo.dcexporter.model.DcModelMapping;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +19,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Service
@@ -37,12 +39,18 @@ public class CkanService {
         return licenses.stream().collect(Collectors.toMap(CkanLicense::getId, CkanLicense::getTitle));
     }
 
-    public CkanDataset getOrCreateDataset(String name, String title) {
+    public Map<String, String> getTags() {
+        CkanClient ckanClient = new CkanClient(ckanUrl);
+        List<CkanTag> tags = ckanClient.getTagList();
+        return tags.stream().collect(Collectors.toMap(CkanTag::getId, CkanTagBase::getName));
+    }
+
+    public CkanDataset getOrCreateDataset(DcModelMapping dcModelMapping) {
         CkanClient ckanClient = new CkanClient(ckanUrl, ckanApiKey);
 
         CkanDataset ckanDataset = null;
         try {
-            ckanDataset = ckanClient.getDataset(name);
+            ckanDataset = ckanClient.getDataset(dcModelMapping.getName());
         } catch (CkanException e) {
             // Not Found Error is an « expected » result
             // FIXME : this is a poor way to perform a search
@@ -50,20 +58,35 @@ public class CkanService {
                 throw e;
             }
 
-            LOGGER.debug("Dataset {} already exists", name);
+            LOGGER.debug("Dataset {} already exists", dcModelMapping.getName());
         }
 
         if (ckanDataset == null) {
             CkanOrganization ckanOrganization = ckanClient.getOrganization("ozwillo");
-            ckanDataset = new CkanDataset(name);
+            ckanDataset = new CkanDataset(dcModelMapping.getName());
             ckanDataset.setOrganization(ckanOrganization);
+            ckanDataset.setMaintainer("ozwillo");
+            ckanDataset.setMaintainerEmail("contact@ozwillo.org");
+            ckanDataset.setOpen(true);
             ckanDataset.setOwnerOrg(ckanOrganization.getId());
-            ckanDataset.setTitle(title);
+            ckanDataset.setTitle(dcModelMapping.getName());
             ckanDataset.setPriv(false);
 
             return ckanClient.createDataset(ckanDataset);
         } else {
-            return ckanDataset;
+            CkanOrganization ckanOrganization = ckanClient.getOrganization("ozwillo");
+            ckanDataset = new CkanDataset(dcModelMapping.getName());
+            ckanDataset.setOrganization(ckanOrganization);
+            ckanDataset.setMaintainer("ozwillo");
+            ckanDataset.setMaintainerEmail("contact@ozwillo.org");
+            ckanDataset.setOwnerOrg(ckanOrganization.getId());
+            ckanDataset.setTitle(dcModelMapping.getResourceName());
+            ckanDataset.setLicenseTitle(dcModelMapping.getLicense());
+            ckanDataset.setUrl(dcModelMapping.getSource());
+            ckanDataset.setVersion(dcModelMapping.getVersion());
+            ckanDataset.setTags(dcModelMapping.getTags());
+            ckanDataset.setPriv(false);
+            return ckanClient.updateDataset(ckanDataset);
         }
     }
 
