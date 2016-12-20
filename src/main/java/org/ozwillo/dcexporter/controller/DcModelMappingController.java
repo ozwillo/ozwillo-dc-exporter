9c1b0effd3d67c64b9448ddc530741ef6b5f8073
OtakuDigitChoc <abdelaliboulajine@gmail.com>
2016-12-20 18:57:05 +0100

    get ckanDataset id and add all data in dcModelMapping

--- src/main/java/org/ozwillo/dcexporter/controller/DcModelMappingController.java
+++ src/main/java/org/ozwillo/dcexporter/controller/DcModelMappingController.java
@@ -1,7 +1,10 @@
 package org.ozwillo.dcexporter.controller;
 
+import eu.trentorise.opendata.jackan.model.CkanDataset;
+import eu.trentorise.opendata.jackan.model.CkanResource;
 import org.ozwillo.dcexporter.dao.DcModelMappingRepository;
 import org.ozwillo.dcexporter.model.DcModelMapping;
+import org.ozwillo.dcexporter.service.CkanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,8 +21,17 @@ public class DcModelMappingController {
     @Autowired
     private DcModelMappingRepository dcModelMappingRepository;
 
+    @Autowired
+    private CkanService ckanService;
+
     @RequestMapping(method = POST)
     public ResponseEntity<String> addMapping(@RequestBody DcModelMapping dcModelMapping) {
+        CkanDataset ckanDataset = ckanService.getOrCreateDataset(dcModelMapping.getName(), dcModelMapping.getName()); //TODO : Remove the capital letter and special caracteres for first parametre before passing
+        CkanResource ckanResource = ckanService.createResource(ckanDataset.getId(), dcModelMapping.getName());
+
+        dcModelMapping.setCkanPackageId(ckanDataset.getId());
+        dcModelMapping.setCkanResourceId(ckanResource.getId());
+
         dcModelMappingRepository.save(dcModelMapping);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
