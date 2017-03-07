ff2a7869b06b310d033ec73848c918f782b66d9f
OtakuDigitChoc <abdelaliboulajine@gmail.com>
2017-03-07 18:00:39 +0100

    [FixUp] ref #53 - Put dropdown right

--- src/main/resources/public/jsx/Dashboard.jsx
+++ src/main/resources/public/jsx/Dashboard.jsx
@@ -5,13 +5,13 @@ import {DropdownButton, MenuItem} from 'react-bootstrap'
 import { ContainerPanel, PanelGroup, Panel } from './Panel'
 
 export default class Dashboard extends React.Component {
+    state = {
+        logs: [],
+        filterKey: '',
+        filterValue: 'Toutes les synchronisations'
+    }
     constructor(){
         super()
-        this.state = {
-            logs: [],
-            filterKey: '',
-            filterValue: 'Toutes les synchronisations'
-        }
         this.onChangeFilter = this.onChangeFilter.bind(this)
     }
     componentDidMount() {
@@ -43,24 +43,28 @@ export default class Dashboard extends React.Component {
                 <h1>Flux d'activités</h1>
                 {renderIf(this.state.logs.length > 0) (
                     <div>
-                        <DropdownButton title={ this.state.filterValue } onSelect={(eventKey) => this.onChangeFilter(eventKey)} id="filter-dropdown" >
-                            <MenuItem eventKey={[ "Toutes les synchronisations", "" ]} >Toutes les synchronisations</MenuItem>
-                            <MenuItem eventKey={[ "Synchronisations réussies", "valid" ]} >Synchronisations réussies</MenuItem>
-                            <MenuItem eventKey={[ "Synchronisations non réussies", "error" ]}  >Synchronisations non réussies</MenuItem>
-                            <MenuItem eventKey={[ "Synchronisations en cours", "pending" ]} >Synchronisations en cours</MenuItem>
-                        </DropdownButton>
-                        {renderIf(list.length > 0) (
-                            <div>
-                                <ContainerPanel>
-                                    <PanelGroup>{list}</PanelGroup>
-                                </ContainerPanel>
-                            </div>
-                        )}
-                        {renderIf(list.length == 0) (
-                            <div className="alert alert-info text-lowercase" role="alert">
-                                <p><i>Aucune { this.state.filterValue }</i></p>
-                            </div>
-                        )}
+                        <div className="filter-dropdown text-right" >
+                            <DropdownButton title={ this.state.filterValue } onSelect={(eventKey) => this.onChangeFilter(eventKey)} id="filter-dropdown" pullRight={true}>
+                                <MenuItem eventKey={[ "Toutes les synchronisations", "" ]} >Toutes les synchronisations</MenuItem>
+                                <MenuItem eventKey={[ "Synchronisations réussies", "valid" ]} >Synchronisations réussies</MenuItem>
+                                <MenuItem eventKey={[ "Synchronisations non réussies", "error" ]}  >Synchronisations non réussies</MenuItem>
+                                <MenuItem eventKey={[ "Synchronisations en cours", "pending" ]} >Synchronisations en cours</MenuItem>
+                            </DropdownButton>
+                        </div>
+                        <div className="wrap-result">
+                            {renderIf(list.length > 0) (
+                                <div>
+                                    <ContainerPanel>
+                                        <PanelGroup>{list}</PanelGroup>
+                                    </ContainerPanel>
+                                </div>
+                            )}
+                            {renderIf(list.length == 0) (
+                                <div className="alert alert-info" role="alert">
+                                    <p><i>Aucunes <span className="text-lowercase"> { this.state.filterValue } </span></i></p>
+                                </div>
+                            )}
+                        </div>
                     </div>
                 )}
                 {renderIf(this.state.logs.length == 0) (
