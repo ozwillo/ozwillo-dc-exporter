762022b5823828421df15e3bfb12c765a78e4d08
Benoit Orihuela <borihuela@ozwillo.org>
2019-04-28 10:58:35 +0200

    feat: set a max nb of resources to export for each model

--- src/main/java/org/ozwillo/dcexporter/dao/DcModelMappingRepository.java
+++ src/main/java/org/ozwillo/dcexporter/dao/DcModelMappingRepository.java
@@ -3,10 +3,12 @@ package org.ozwillo.dcexporter.dao;
 import org.ozwillo.dcexporter.model.DcModelMapping;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface DcModelMappingRepository extends MongoRepository<DcModelMapping, String> {
 
     DcModelMapping findByDcId(String dcId);
