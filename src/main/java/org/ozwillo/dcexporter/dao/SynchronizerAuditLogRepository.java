08b9d5b76bd0c21c96c88669b6b9971001ee06f4
Benoit Orihuela <borihuela@ozwillo.org>
2019-04-28 17:05:12 +0200

    chore: add @Repositoty annotation for improved IDE support

--- src/main/java/org/ozwillo/dcexporter/dao/SynchronizerAuditLogRepository.java
+++ src/main/java/org/ozwillo/dcexporter/dao/SynchronizerAuditLogRepository.java
@@ -2,9 +2,9 @@ package org.ozwillo.dcexporter.dao;
 
 import org.ozwillo.dcexporter.model.SynchronizerAuditLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
+@Repository
 public interface SynchronizerAuditLogRepository extends MongoRepository<SynchronizerAuditLog, String> {
 
     SynchronizerAuditLog findFirstByTypeOrderByDateDesc(String type);
